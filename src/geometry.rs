93eaff45d12bede282ac5a10c402c9798fe6082e
Luo <gnulinux@126.com>
2016-06-14 08:07:39 +0800

    .

--- src/geometry.rs
+++ src/geometry.rs
@@ -7,7 +7,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-pub use base::CGFloat;
+use base::CGFloat;
 
 #[repr(C)]
 pub struct CGSize {
